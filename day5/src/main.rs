7c9ac9d501c362ad62bb77acaabba569365a4612
James Derrick <jamesgordonderrick@gmail.com>
2023-12-05 18:22:18 +0000

    added more tests; the bug still persists - not sure why

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,7 +1,7 @@
 use itertools::izip;
 use std::collections::HashMap;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 enum Overlap {
     PartialLeft,
     PartialRight,
@@ -19,16 +19,17 @@ struct Range2 {
 impl Range2 {
     fn overlap(&self, other: Range2) -> Overlap{
         let a0: usize = self.start;
-        let a1: usize = self.start + self.size;
+        let a1: usize = self.start + self.size - 1;
         let b0: usize = other.start;
-        let b1: usize = other.start + other.size;
+        let b1: usize = other.start + other.size - 1;
+        // dbg!(a0, a1, b0, b1);
         if a0 > b1 || b0 > a1 {
             return Overlap::None;
         }
-        else if a0 < b0 && a1 < b1 {
+        else if a0 <= b0 && a1 < b1 {
             return Overlap::PartialLeft;
         }
-        else if a0 > b0 && a1 > b1 {
+        else if a0 > b0 && a1 >= b1 {
             return  Overlap::PartialRight;
         }
         else if a0 > b0 && a1 < b1 {
@@ -38,25 +39,8 @@ impl Range2 {
             return Overlap::Total;
         }
         else {
-            return Overlap::None;
+            unreachable!();
         }
-        // if self.start <= other.start + other.size && self.start + self.size >= other.start{
-        //     if self.start > other.start && self.start + self.size < other.start + other.size {
-        //         return Overlap::Total;
-        //     }
-        //     else {
-        //         if self.start < other.start && other.start >= self.start+self.size {
-        //             return Overlap::PartialLeft;
-        //         }
-        //         else if self.start > other.start && other.start <= self.start+self.size{
-        //             return Overlap::PartialRight;
-        //         }
-        //         else if self.start < other.start && self.start + self.size > other.start + other.size {
-        //             return Overlap::PartialMiddle;
-        //         }
-        //     }
-        // }
-        // return Overlap::None;
     }
     fn map_to_partial_left(&self, other: Range2, out: Range2) -> Vec<Range2> {
         let a0: usize = self.start;
@@ -65,6 +49,7 @@ impl Range2 {
         let b1: usize = other.start + other.size;
         let trunc_range: Range2 = Range2 { start: a1, size: b1-a1 };
         let mapped_range: Range2 = Range2 { start: b0-a0+out.start, size: a1-b0 };
+        assert_eq!(trunc_range.size+mapped_range.size, other.size);
         return vec![trunc_range, mapped_range];
     }
     fn map_to_partial_right(&self, other: Range2, out: Range2) -> Vec<Range2> {
@@ -73,6 +58,7 @@ impl Range2 {
         let b1: usize = other.start + other.size;
         let trunc_range: Range2 = Range2 { start: b0, size: a0-b0 };
         let mapped_range: Range2 = Range2 { start: out.start, size: b1-a0 };
+        assert_eq!(trunc_range.size+mapped_range.size, other.size);
         return vec![trunc_range, mapped_range];
     }
     fn map_to_partial_middle(&self, other: Range2, out: Range2) -> Vec<Range2> {
@@ -83,12 +69,14 @@ impl Range2 {
         let trunc_range1: Range2 = Range2 { start: b0, size: a0-b0 };
         let trunc_range2: Range2 = Range2 { start: a1, size: b1-a1 };
         let mapped_range: Range2 = Range2 { start: out.start, size: out.size };
+        assert_eq!(trunc_range1.size+trunc_range2.size+mapped_range.size, other.size);
         return vec![trunc_range1, trunc_range2, mapped_range];
     }
     fn map_to_total(&self, other: Range2, out: Range2) -> Vec<Range2> {
         let a0: usize = self.start;
         let b0: usize = other.start;
         let mapped_range: Range2 = Range2 { start: b0-a0+out.start, size: other.size };
+        assert_eq!(mapped_range.size, other.size);
         return vec![mapped_range];
     }
 }
@@ -118,10 +106,10 @@ struct AlmanacMap {
 impl MultiRange {
     fn get_lowest_value(&self) -> usize {
         let mut value: usize = usize::MAX;
+        // if we filter out values equal to zero it works for the input
+        // if not it only works for the example. Why??
         for range2 in self.ranges.clone() {
-            if range2.start > 0 {
-                value = value.min(range2.start);
-            }
+            value = value.min(range2.start);
         }
         value
     }
@@ -153,15 +141,16 @@ impl AlmanacMap {
 impl AlmanacMap2 {
     fn map_ranges_to_ranges(&self, ranges: MultiRange) -> MultiRange{
         let mut new_ranges: Vec<Range2> = Vec::new();
+        let mut overlap: Overlap;
         for range2 in ranges.ranges.clone(){
             for (in_range, out_range) in izip!(self.in_range.ranges.clone(), self.out_range.ranges.clone()) {
-                match in_range.overlap(range2.clone()){
+                overlap = in_range.overlap(range2.clone());
+                match overlap {
                     Overlap::Total => new_ranges.append(&mut in_range.map_to_total(range2.clone(), out_range)),
                     Overlap::PartialLeft => new_ranges.append(&mut in_range.map_to_partial_left(range2.clone(), out_range)),
                     Overlap::PartialRight => new_ranges.append(&mut in_range.map_to_partial_right(range2.clone(), out_range)),
                     Overlap::PartialMiddle => new_ranges.append(&mut in_range.map_to_partial_middle(range2.clone(), out_range)),
                     Overlap::None => (),
-                    
                 }
             }
             if new_ranges.len() == 0 {
@@ -305,12 +294,39 @@ pub fn test_partial_right() {
     let out: Range2 = Range2 { start: 90, size: 2 };
     let results: Vec<Range2>;
     results = a.map_to_partial_right(b, out);
-    dbg!(results.clone());
     assert_eq!(results.len(), 2);
     assert_eq!(results[0].start, 40);
     assert_eq!(results[0].size, 1);
     assert_eq!(results[1].start, 90);
     assert_eq!(results[1].size, 2);
+}
+
+#[test]
+pub fn test_partial_right_zeros1() {
+    let a: Range2 = Range2 { start: 41, size: 2 };
+    let b: Range2 = Range2 { start: 40, size: 3 };
+    let out: Range2 = Range2 { start: 0, size: 2 };
+    let results: Vec<Range2>;
+    results = a.map_to_partial_right(b, out);
+    assert_eq!(results.len(), 2);
+    assert_eq!(results[0].start, 40);
+    assert_eq!(results[0].size, 1);
+    assert_eq!(results[1].start, 0);
+    assert_eq!(results[1].size, 2);
+}
+
+#[test]
+pub fn test_partial_left_one_value() {
+    let a: Range2 = Range2 { start: 3, size: 3 };
+    let b: Range2 = Range2 { start: 5, size: 3 };
+    let out: Range2 = Range2 { start: 0, size: 3 };
+    let results: Vec<Range2>;
+    results = a.map_to_partial_left(b, out);
+    assert_eq!(results.len(), 2);
+    assert_eq!(results[0].start, 6);
+    assert_eq!(results[0].size, 2);
+    assert_eq!(results[1].start, 2);
+    assert_eq!(results[1].size, 1);
 }
 
 #[test]
@@ -320,7 +336,6 @@ pub fn test_partial_left() {
     let out: Range2 = Range2 { start: 90, size: 5 };
     let results: Vec<Range2>;
     results = a.map_to_partial_left(b, out);
-    dbg!(results.clone());
     assert_eq!(results.len(), 2);
     assert_eq!(results[0].start, 46);
     assert_eq!(results[0].size, 6);
@@ -328,6 +343,29 @@ pub fn test_partial_left() {
     assert_eq!(results[1].size, 4);
 }
 
+#[test]
+pub fn test_total2() {
+    let a: Range2 = Range2 { start: 0, size: 10 };
+    let b: Range2 = Range2 { start: 0, size: 3 };
+    let out: Range2 = Range2 { start: 90, size: 10 };
+    let results: Vec<Range2>;
+    results = a.map_to_total(b, out);
+    assert_eq!(results.len(), 1);
+    assert_eq!(results[0].start, 90);
+    assert_eq!(results[0].size, 3);
+}
+
+#[test]
+pub fn test_total3() {
+    let a: Range2 = Range2 { start: 0, size: 10 };
+    let b: Range2 = Range2 { start: 8, size: 3 };
+    let out: Range2 = Range2 { start: 90, size: 10 };
+    let results: Vec<Range2>;
+    results = a.map_to_total(b, out);
+    assert_eq!(results.len(), 1);
+    assert_eq!(results[0].start, 98);
+    assert_eq!(results[0].size, 3);
+}
 
 #[test]
 pub fn test_total() {
@@ -336,19 +374,45 @@ pub fn test_total() {
     let out: Range2 = Range2 { start: 90, size: 5 };
     let results: Vec<Range2>;
     results = a.map_to_total(b, out);
-    dbg!(results.clone());
     assert_eq!(results.len(), 1);
     assert_eq!(results[0].start, 91);
     assert_eq!(results[0].size, 3);
 }
 
+#[test]
+pub fn test_overlap() {
+    let a = Range2 { start: 3, size: 3 }; // 3, 4, 5
+    let b = Range2 { start: 6, size: 3 }; // 6, 7, 8
+    let c = Range2 { start: 3, size: 6 }; // 3, 4, 5, 6, 7, 8
+    let d = Range2 { start: 4, size: 3 }; // 4, 5, 6
+    let e: Range2 = Range2 { start: 6, size: 3 }; // 6, 7, 8
+
+    // a has no overlap with b
+    assert_eq!(a.overlap(b.clone()), Overlap::None);
+    assert_eq!(b.overlap(a.clone()), Overlap::None);
+    // a is fully contained in c
+    assert_eq!(a.overlap(c.clone()), Overlap::PartialLeft);
+    // d and e overlap by a single value
+    assert_eq!(d.overlap(e.clone()), Overlap::PartialLeft);
+    // d and e overlap by a single value reversed
+    assert_eq!(e.overlap(d.clone()), Overlap::PartialRight);
+    // b is fully contained in c
+    assert_eq!(b.overlap(c.clone()), Overlap::PartialRight);
+    // c overlaps fully with a but has more to the right
+    assert_eq!(c.overlap(a.clone()), Overlap::Total);
+    // c overlaps fully with b but has more to the left
+    assert_eq!(c.overlap(b.clone()), Overlap::Total);
+    // d overlaps partially with a but has more to the right
+    assert_eq!(d.overlap(a.clone()), Overlap::PartialRight);
+    // a overlaps partially with d but has more to the left
+    assert_eq!(a.overlap(d.clone()), Overlap::PartialLeft);
+    // d overlaps with c and produces 3 ranges
+    assert_eq!(d.overlap(c.clone()), Overlap::PartialMiddle);
+    // d overlaps with c reverse
+    assert_eq!(c.overlap(d.clone()), Overlap::Total);
+}
+
 fn main() {
-    // let a: Range2 = Range2 { start: 41, size: 3 };
-    // let b: Range2 = Range2 { start: 40, size: 3 };
-    // let out: Range2 = Range2 { start: 90, size: 3 };
-    // let results: Vec<Range2>;
-    // results = a.map_to_partial_right(b, out);
-    // dbg!(results.clone());
     part1();
     part2();
 }
