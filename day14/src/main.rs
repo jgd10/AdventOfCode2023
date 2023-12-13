97c059188174b50954bce4e13b11f6b0f1127a26
James Derrick <jamesgordonderrick@gmail.com>
2023-12-13 17:54:45 +0000

    day 14 ready to return to in Rust

---
+++ day14/src/main.rs
@@ -0,0 +1,17 @@
+mod aoc_parser;
+use aoc_parser::get_input_as_lines;
+
+
+fn part1(){
+    println!("Part 1 Answer: {}", 0);
+}
+
+
+fn part2(){
+    println!("Part 2 Answer: {}", 0);
+}
+
+fn main() {
+    part1();
+    part2();
+}
