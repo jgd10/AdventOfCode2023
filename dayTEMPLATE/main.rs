2f59de911c42ab784bb24aa376eccadfa3416505
James Derrick <jamesgordonderrick@gmail.com>
2023-12-18 08:56:17 +0000

    added timers

--- dayTEMPLATE/main.rs
+++ dayTEMPLATE/main.rs
@@ -1,4 +1,5 @@
 mod aoc_parser;
+use std::time::Instant;
 
 
 fn parse_input() {
@@ -15,6 +16,10 @@ fn part2(){
 }
 
 fn main() {
+    let start = Instant::now();
     part1();
+    println!("*** Part 1 Took {:.2?} ***", start.elapsed());
+    let start2 = Instant::now();
     part2();
+    println!("*** Part 2 Took {:.2?} ***", start2.elapsed());
 }
