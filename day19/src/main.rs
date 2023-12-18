2868294dcfd029eacc6684b2f9048d38e8c7c60e
James Derrick <jamesgordonderrick@gmail.com>
2023-12-18 15:56:52 +0000

    fixed template to work and prep for day19

---
+++ day19/src/main.rs
@@ -0,0 +1,25 @@
+mod aoc_parser;
+use std::time::Instant;
+
+
+fn parse_input() {
+
+}
+
+fn part1(){
+    println!("Part 1 Answer: {}", 0);
+}
+
+
+fn part2(){
+    println!("Part 2 Answer: {}", 0);
+}
+
+fn main() {
+    let start = Instant::now();
+    part1();
+    println!("*** Part 1 Took {:.2?} ***", start.elapsed());
+    let start2 = Instant::now();
+    part2();
+    println!("*** Part 2 Took {:.2?} ***", start2.elapsed());
+}
