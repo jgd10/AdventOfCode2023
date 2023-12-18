1e9919a68fb82ff46f7c710563f0c4493d85a415
James Derrick <jamesgordonderrick@gmail.com>
2023-12-18 09:26:57 +0000

    changed code to use functools cache memoisation instead of writing it out manually

--- day12/src/main.py
+++ day12/src/main.py
@@ -1,3 +1,4 @@
+import functools
 from dataclasses import dataclass
 import time
 
@@ -8,48 +9,38 @@ class PuzzleLine:
     clues: list[int]
 
 
-def get_number_of_variants(line: str, clues: list[int], cache: dict[tuple[str, tuple[int]], int]) -> int:
-    key = (line, tuple(clues))
-    if key in cache:
-        return cache[key]
+@functools.cache
+def get_number_of_variants(line: str, clues: tuple[int]) -> int:
+    clues = list(clues)
     num = 0
     if not line and clues:
-        cache[key] = 0
         return 0
     if not clues and line:
         if '#' in line:
-            cache[key] = 0
             return 0
         else:
-            cache[key] = 1
             return 1
     if len(line) < sum(clues) + len(clues) - 1:
-        cache[key] = 0
         return 0
     if not line and not clues:
-        cache[key] = 1
         return 1
     if line.startswith('.'):
-        num = get_number_of_variants(line[1:], clues, cache)
+        num = get_number_of_variants(line[1:], tuple(clues))
     elif line.startswith('#'):
         if {c for c in line[:clues[0]]}.issubset({'#', '?'}):
             if clues[0] > len(line):
-                cache[key] = 0
                 return 0
             elif clues[0] == len(line):
                 return 1
             elif line[clues[0]] == '#':
-                cache[key] = 0
                 return 0
             else:
-                num = get_number_of_variants(line[clues[0]+1:], clues[1:], cache)
+                num = get_number_of_variants(line[clues[0]+1:], tuple(clues[1:]))
         else:
-            cache[key] = 0
             return 0
     elif line.startswith('?'):
-        num = get_number_of_variants('.' + line[1:], clues, cache) \
-              + get_number_of_variants('#' + line[1:], clues, cache)
-    cache[key] = num
+        num = get_number_of_variants('.' + line[1:], tuple(clues)) \
+              + get_number_of_variants('#' + line[1:], tuple(clues))
     return num
 
 
@@ -71,7 +62,7 @@ def part2():
         counts = [int(c) for c in count_str.split(',')]*5
         lines.append(PuzzleLine(springs, counts))
     for i, line in enumerate(lines):
-        counter = get_number_of_variants(line.springs, line.clues, {})
+        counter = get_number_of_variants(line.springs, tuple(line.clues))
         total += counter
     print(f'Part 2 Answer = {total}')
 
@@ -85,7 +76,7 @@ def part1():
         lines.append(PuzzleLine(spring_str, counts))
     total = 0
     for i, line in enumerate(lines):
-        counter = get_number_of_variants(line.springs, line.clues, {})
+        counter = get_number_of_variants(line.springs, tuple(line.clues))
         total += counter
     print(f'Part 1 Answer = {total}')
 
@@ -95,7 +86,7 @@ def test_input():
     expected = [2, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0]
     clues = [[2], [2], [2], [2], [2], [2], [2], [2], [2], [2, 1]]
     for expect, line, clue in zip(expected, lines, clues):
-        n = get_number_of_variants(line, clue, {})
+        n = get_number_of_variants(line, tuple(clue))
         print(line, clue, f'Found {n}, Expected {expect}')
 
 
