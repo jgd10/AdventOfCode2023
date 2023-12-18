b709242225a8b13dadc645cb9454974959d76bc8
James Derrick <jamesgordonderrick@gmail.com>
2023-12-18 09:25:52 +0000

    added some tentative helper functions from day18

--- dayTEMPLATE/aoc_parser.rs
+++ dayTEMPLATE/aoc_parser.rs
@@ -1,3 +1,4 @@
+use std::collect::{BTreeSet, VecDeque};
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord, Hash)]
 #[allow(dead_code)]
@@ -29,6 +30,57 @@ pub struct Coord32 {
 pub struct Coord64 {
     pub x: i64,
     pub y: i64,
+}
+
+// Uses a rule I found by accident: get the total area using the shoelace algorithm (the trapezoid formula in this case)
+// Then add that to half the total perimeter and add 1.
+// Taken from 2023 day 18 - only works for polygon with either horizontal or vertical edges (i.e. squares)
+#[allow(dead_code)]
+fn calculate_area_polygon32(coordinates: Vec<Coord32> | VecDeque<Coord32>) -> i32{
+    let mut total: i32 = 0;
+    for i in 0..(coordinates.len()-1){
+        let amount = (vertices[i].y + vertices[i+1].y)*(vertices[i].x - vertices[i+1].x);
+        total += amount;
+    }
+    total += (vertices[vertices.len()-1].y + vertices[0].y)*(vertices[vertices.len()-1].x - vertices[0].x);
+    total.abs() / 2 + calculate_perimeter32(vertices) / 2 + 1
+}
+
+
+// Taken from 2023 day 18 - only works for polygon with either horizontal or vertical edges
+#[allow(dead_code)]
+fn calculate_area_polygon32(coordinates: Vec<Coord64> | VecDeque<Coord64>) -> i64{
+    let mut total: i64 = 0;
+    for i in 0..(coordinates.len()-1){
+        let amount = (vertices[i].y + vertices[i+1].y)*(vertices[i].x - vertices[i+1].x);
+        total += amount;
+    }
+    total += (vertices[vertices.len()-1].y + vertices[0].y)*(vertices[vertices.len()-1].x - vertices[0].x);
+    total.abs() / 2 + calculate_perimeter64(vertices) / 2 + 1
+}
+
+// Taken from 2023 day 18 - only works for polygon with either horizontal or vertical edges
+#[allow(dead_code)]
+fn calculate_perimeter32(vertices: Vec<Coord32> | VecDeque<Coord32>) -> i32 {
+    let mut total: i32 = 0;
+    for i in 0..(coordinates.len()-1){
+        let amount = (vertices[i].y - vertices[i+1].y)+(vertices[i].x - vertices[i+1].x);
+        total += amount;
+    }
+    total += (vertices[vertices.len()-1].y + vertices[0].y)*(vertices[vertices.len()-1].x - vertices[0].x);
+    total
+}
+
+// Taken from 2023 day 18 - only works for polygon with either horizontal or vertical edges
+#[allow(dead_code)]
+fn calculate_perimeter64(vertices: Vec<Coord64> | VecDeque<Coord64>) -> i64 {
+    let mut total: i64 = 0;
+    for i in 0..(coordinates.len()-1){
+        let amount = (vertices[i].y - vertices[i+1].y)+(vertices[i].x - vertices[i+1].x);
+        total += amount;
+    }
+    total += (vertices[vertices.len()-1].y + vertices[0].y)*(vertices[vertices.len()-1].x - vertices[0].x);
+    total
 }
 
 #[allow(dead_code)]
